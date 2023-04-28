fa8ec3c196ad683973bef7681b0737323c3a8188
kuboya <k.kuboya@gmail.com>
2023-04-28 23:26:35 +0900

    add CountDown and Counter

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { ChakraProvider } from '@chakra-ui/react';
 import App from 'App';
+import CountDown from 'components/CountDown';
+import Counter from 'components/Counter';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <ChakraProvider>
       <App />
+      <Counter />
+      <CountDown />
     </ChakraProvider>
   </React.StrictMode>
 );
