5e31c8ae627ccb9132b4da508b94465fd060f2f0
kuboya <k.kuboya@gmail.com>
2023-04-28 22:00:40 +0900

    add sample01

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,49 @@
-import { type FC, useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from './assets/vite.svg';
+import type { FC } from 'react';
+import { Heading } from '@chakra-ui/react';
+import CharacterList from 'components/CharacterList';
+import type { Character } from 'components/CharacterList';
 import './App.css';
 
-const title = import.meta.env.VITE_APP_TITLE;
-console.dir(import.meta.env);
-
 const App: FC = () => {
-  const [count, setCount] = useState(0);
+  const characters: Character[] = [
+    {
+      id: 1,
+      name: '鈴木武',
+      grade: 1,
+      height: 190,
+    },
+    {
+      id: 2,
+      name: '木戸川猿',
+      grade: 1,
+      height: 230,
+    },
+    {
+      id: 3,
+      name: '牧村晴治',
+      grade: 2,
+      height: 160,
+    },
+    {
+      id: 4,
+      name: '小園三四郎',
+      grade: 3,
+    },
+    {
+      id: 5,
+      name: '東正三',
+      grade: 3,
+      height: 198,
+    },
+  ];
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>{title}</h1>
-      <div className="card">
-        <button
-          onClick={() => {
-            setCount((count) => count + 1);
-          }}
-        >
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="container">
+      <Heading as="h1" size="2xl" my={12}>
+        山一商事じゃんけん大会
+      </Heading>
+      <CharacterList domain="業販第３本部" characters={characters} />
+    </div>
   );
 };
 
